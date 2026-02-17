2b7481df41143d630f60910b4b4f484d575c1997
saltex-extra <154666574+saltex-extra@users.noreply.github.com>
2026-02-17 15:16:04 +0900

    解説AC 典型90問001

---
+++ 典型90問/001.py
@@ -0,0 +1,29 @@
+N, L = map(int, input().split())
+K = int(input())
+A = list(map(int, input().split()))
+
+def check(x):
+    num = 0
+    pre = 0
+
+    for i in range(N):
+        if A[i] - pre >= x:
+            num += 1
+            pre = A[i]
+
+    if L - pre >= x:
+        num += 1
+
+    return (num >= K + 1)
+
+left = -1
+right = L + 1
+
+while right - left > 1:
+    mid = (right + left) // 2
+    if check(mid):
+        left = mid
+    else:
+        right = mid
+
+print(left)
